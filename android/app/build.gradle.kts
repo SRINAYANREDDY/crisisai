65577287cdabce72fe698a5830d0e947b3ca04f5
SRINAYANREDDY <r.srinayanreddy23@gmail.com>
2026-04-22 00:47:26 +0530

    Update namespace and applicationId for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.crisisai"
+    namespace = "com.example.crisis_ai"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.crisisai"
+        applicationId = "com.example.crisis_ai"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
