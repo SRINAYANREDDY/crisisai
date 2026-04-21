8c8f50386e5609dfac0bb832146b07b6c27903a9
SRI NAYAN REDDY <r.srinayanreddy23@gmail.com>
2026-04-22 00:09:43 +0530

    fix: compileSdk 36, upgrade speech_to_text to 7.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.crisisai"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "28.2.13676358"
 
     compileOptions {
